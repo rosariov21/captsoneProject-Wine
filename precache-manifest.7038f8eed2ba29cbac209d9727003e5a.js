e8defb0b4aa627fd98d42aba9bd898169edb9f05
rosariov21 <rosariovelazquez@Rosarios-MBP.home>
2020-09-23 13:09:30 -0400

    Updates

---
+++ precache-manifest.7038f8eed2ba29cbac209d9727003e5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529b849359ab96dda9e772372adee638",
+    "url": "/captsoneProject-Wine/index.html"
+  },
+  {
+    "revision": "38f3d63431bd2392ed71",
+    "url": "/captsoneProject-Wine/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "8ac0367198e8cf993cbc",
+    "url": "/captsoneProject-Wine/static/css/main.085b6776.chunk.css"
+  },
+  {
+    "revision": "38f3d63431bd2392ed71",
+    "url": "/captsoneProject-Wine/static/js/2.dbcb4d70.chunk.js"
+  },
+  {
+    "revision": "e5da79b2739e1ff9be7b8235ac5aeca2",
+    "url": "/captsoneProject-Wine/static/js/2.dbcb4d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac0367198e8cf993cbc",
+    "url": "/captsoneProject-Wine/static/js/main.741d0f71.chunk.js"
+  },
+  {
+    "revision": "f8c77ee7481e03a09347",
+    "url": "/captsoneProject-Wine/static/js/runtime-main.16e7b4be.js"
+  },
+  {
+    "revision": "894d68813c08e253bd1fd389af4f9432",
+    "url": "/captsoneProject-Wine/static/media/WCart.894d6881.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/captsoneProject-Wine/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1141b582deec3ac1c63fded38c14af06",
+    "url": "/captsoneProject-Wine/static/media/red.1141b582.jpg"
+  }
+]);
